57f94aef40ff74e95b26ea56933731d8b43309b9
root <root@rhel.dockerdc.fr>
2017-11-11 21:10:13 +0100

    update script docker_dm

--- docker_dm.sh
+++ docker_dm.sh
@@ -15,7 +15,7 @@ rpm -Uvh https://storebits.docker.com/ee/centos/sub-43f20f5b-6abf-448e-952a-6f17
 rpm -Uvh https://storebits.docker.com/ee/centos/sub-43f20f5b-6abf-448e-952a-6f1741b31d6e/centos/7/x86_64/stable-17.03/Packages/docker-ee-17.03.2.ee.7-1.el7.centos.x86_64.rpm
 #### Configuration docker
 
-sed -i -e "s/ExecStart/ExecStart=\/usr\/bin\/dockerd --storage-driver=devicemapper --storage-opt=dm.thinpooldev=\/dev\/mapper\/data-pooldata --storage-opt dm.use_deferred_removal=true/g" /usr/lib/systemd/system/docker.service
+sed -i -e "s/ExecStart/ExecStart=\/usr\/bin\/dockerd --storage-driver=devicemapper --storage-opt=dm.thinpooldev=\/dev\/mapper\/data-pooldata --storage-opt dm.use_deferred_removal=true --storage-opt dm.use_deferred_deletion=true/g" /usr/lib/systemd/system/docker.service
 systemctl daemon-reload
 systemctl start docker
 systemctl status docker
